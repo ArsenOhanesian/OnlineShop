bfd61bdf3bdea0fb216cdca0961e8fc70aa527cd
ArsenOhanesian <teddyyout21@gmail.com>
2024-08-05 11:27:11 +0300

    Initial Commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by csd_setup on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
